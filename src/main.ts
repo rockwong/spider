d8e78e721e3a64b66b8aee006568528da4da8782
fei.wang <fei.wang@loncus.com>
2018-12-19 17:57:25 +0800

    format code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import * as http from "http";
-import { demo } from "./demo/demo";
+import * as http from 'http';
+import { demo } from './demo/demo';
 
 function main() {
-  console.log("test node ts12");
+  console.log('test node ts12');
   demo();
 }
 
@@ -10,15 +10,15 @@ main();
 // ====================
 const server = http.createServer(function(
   request: http.IncomingMessage,
-  response: http.ServerResponse
+  response: http.ServerResponse,
 ): void {
-  console.log("create a server...");
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Hello world,we use typescript to develop.");
+  console.log('create a server...');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Hello world,we use typescript to develop.');
   response.end();
 });
 
 server.listen(3000, function() {
-  console.log("Server listening on port 3000");
-  console.log("test...");
+  console.log('Server listening on port 3000');
+  console.log('test...');
 });
